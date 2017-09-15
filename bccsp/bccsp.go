573b77210a56c1a6d0aa69e2968fc0381736eac3
mxz-scott <maoxiuze@tj-fintech.com>
2017-09-15 12:58:52 +0800

    update comment

--- bccsp/bccsp.go
+++ bccsp/bccsp.go
@@ -1,12 +1,9 @@
 /*
 Copyright IBM Corp. 2016 All Rights Reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
 		 http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -100,33 +97,26 @@ type DecrypterOpts interface{}
 type BCCSP interface {
 
 	// KeyGen generates a key using opts.
-	//根据key生成选项opts生成一个key
-	//与key有关的选项opts选项要适合原始的key（与“证书是一级一级的认证”对看）
 	KeyGen(opts KeyGenOpts) (k Key, err error)
 
 	// KeyDeriv derives a key from k using opts.
 	// The opts argument should be appropriate for the primitive used.
-	//根据key获取选项opts从k中重新获取一个key
 	KeyDeriv(k Key, opts KeyDerivOpts) (dk Key, err error)
 
 	// KeyImport imports a key from its raw representation using opts.
 	// The opts argument should be appropriate for the primitive used.
-	//根据key导入选项opts从一个key原始的数据中导入一个key
 	KeyImport(raw interface{}, opts KeyImportOpts) (k Key, err error)
 
 	// GetKey returns the key this CSP associates to
 	// the Subject Key Identifier ski.
-	//根据SKI返回与该接口实例有联系的key
 	GetKey(ski []byte) (k Key, err error)
 
 	// Hash hashes messages msg using options opts.
 	// If opts is nil, the default hash function will be used.
-	//根据哈希选项opts哈希一个消息msg，如果opts为空，则使用默认选项
 	Hash(msg []byte, opts HashOpts) (hash []byte, err error)
 
 	// GetHash returns and instance of hash.Hash using options opts.
 	// If opts is nil, the default hash function will be returned.
-	//根据哈希选项opts获取hash.Hash实例，如果opts为空，则使用默认选项
 	GetHash(opts HashOpts) (h hash.Hash, err error)
 
 	// Sign signs digest using key k.
@@ -135,22 +125,17 @@ type BCCSP interface {
 	// Note that when a signature of a hash of a larger message is needed,
 	// the caller is responsible for hashing the larger message and passing
 	// the hash (as digest).
-	//根据签名者选项opts，使用k对digest进行签名，注意如果需要对一个特别大的消息的hash值
-	//进行签名，调用者则负责对该特别大的消息进行hash后将其作为digest传入
 	Sign(k Key, digest []byte, opts SignerOpts) (signature []byte, err error)
 
 	// Verify verifies signature against key k and digest
 	// The opts argument should be appropriate for the algorithm used.
-	//根据鉴定者选项opts，通过对比k和digest，鉴定签名
 	Verify(k Key, signature, digest []byte, opts SignerOpts) (valid bool, err error)
 
 	// Encrypt encrypts plaintext using key k.
 	// The opts argument should be appropriate for the algorithm used.
-	//根据加密者选项opts，使用k加密plaintext
 	Encrypt(k Key, plaintext []byte, opts EncrypterOpts) (ciphertext []byte, err error)
 
 	// Decrypt decrypts ciphertext using key k.
 	// The opts argument should be appropriate for the algorithm used.
-	//根据解密者选项opts，使用k对ciphertext进行解密
 	Decrypt(k Key, ciphertext []byte, opts DecrypterOpts) (plaintext []byte, err error)
 }
